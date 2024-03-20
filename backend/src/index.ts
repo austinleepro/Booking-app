c09ec80854d3ccd904423c0f5b125faea1c87a91
austinleepro <austinlee.kinddev@gmail.com>
2024-03-19 18:41:13 -0700

    setting up backend

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cors());
+
+// get test api
+app.get("/api/test", async (req : Request, res : Response) => {
+    res.json({message: "Hello from express endpoint!"})
+});
+
+// app listening
+app.listen(7000, () => {
+    console.log("App is running on localhost:7000");
+});
