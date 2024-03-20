742db548992ccc5e8f57ef657c7af16ee1abd4b6
austinleepro <austinlee.kinddev@gmail.com>
2024-03-20 12:20:45 -0700

    working on register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Navigate
 } from "react-router-dom"
 import Layout from './layouts/Layout'
+import Register from "./pages/Register"
 
 const App =() => {
   return (
@@ -25,6 +26,14 @@ const App =() => {
               <p>Search page</p>
             </Layout>
           } 
+        />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
         />
         <Route 
           path="*" 
