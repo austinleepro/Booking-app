8698dc30f86ee0096140bbbc29f1ef20bfdd13c4
austinleepro <austinlee.kinddev@gmail.com>
2024-03-25 09:51:34 -0700

    finish the auth test for both sign in & register

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
   firstName: string;
@@ -26,7 +26,7 @@ const Register = () => {
 
   const mutation = useMutation(apiClient.register, {
     onSuccess: async () => {
-      showToast({ message: "Registeration Success!", type: "SUCCESS" });
+      showToast({ message: "Registration Successful!", type: "SUCCESS" });
       await queryClient.invalidateQueries("validateToken");
       navigate("/");
     },
@@ -115,7 +115,14 @@ const Register = () => {
           <span className="text-red-500">{errors.confirmPassword.message}</span>
         )}
       </label>
-      <span>
+      <span className="flex items-center justify-between">
+        <span className="text-sm">
+          Already registered?{" "}
+          <Link className="underline" to="/sign-in">
+            {" "}
+            Sign in here
+          </Link>
+        </span>
         <button
           type="submit"
           className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-x1"
